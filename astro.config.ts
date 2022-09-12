d09fe4ff61f5391d91c88d3019453ce7dc400ff3
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-09-13 00:16:03 +0200

    Added react and react icons

--- astro.config.ts
+++ astro.config.ts
@@ -3,19 +3,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	markdown: {
-		syntaxHighlight: 'prism',
-	},
-	site: "https://blog.ratulmaharaj.com",
-	integrations: [
-		mdx({
-		}),
-		tailwind({
-			config: { applyBaseStyles: false },
-		}),
-		image(),
-		sitemap(),
-	],
+  markdown: {
+    syntaxHighlight: 'prism'
+  },
+  site: "https://blog.ratulmaharaj.com",
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), sitemap(), react()]
 });
